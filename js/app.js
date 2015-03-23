0a9368c7c6860cb078d2e88eba295db709cfb7f3
Lucas Lessa <lucaslessa@me.com>
2015-03-23 05:43:10 -0300

    Some changes to Cat Clickr Premium

--- js/app.js
+++ js/app.js
@@ -1,15 +1,48 @@
 $(function() {
-    var cats = {};
-    cats.a = 0;
-    cats.b = 0;
-    $('#cat1').click(function() {
-        cats.a += 1;
-        console.log(cats);
-        $(this).find('.cont-numb').html(cats.a);
+    /* cats = lista os gatos em um array de objetos */
+    var cats = [{
+        'name': 'Hero',
+        'count': 0,
+        'img': 'img/cat.jpg',
+    }, {
+        'name': 'Master',
+        'count': 0,
+        'img': 'img/cat2.jpg',
+    }, {
+        'name': 'Lucky',
+        'count': 0,
+        'img': 'img/cat3.jpg',
+    }, {
+        'name': 'Sleepy',
+        'count': 0,
+        'img': 'img/cat4.jpg',
+    }, {
+        'name': 'Smarty',
+        'count': 0,
+        'img': 'img/cat5.jpg',
+    }];
+    /* Percorre o array cats e cria os elementos do menu .cat-menu */
+    for (var i = 0; i < cats.length; i++) {
+        $('.cat-menu').prepend('<a href="#" cat-id="' + i + '" class="list-group-item">' + cats[i].name + '</a>');
+    };
+    /* Chama a função mostraGato com base na cat-id do link clicado */
+    $('.cat-menu').find('a').click(function(e) {
+        var catID = $(this).attr('cat-id');
+        mostraGato(catID);
+        e.preventDefault();
     });
-    $('#cat2').click(function() {
-        cats.b += 1;
-        console.log(cats);
-        $(this).find('.cont-numb').html(cats.b);
-    });
+    /* função mostraGato = recebe um id do gato selecionado e insere no elemento #cat da página */
+    var mostraGato = function(catID) {
+            $('#cat').html('<img cat-id="' + catID + '" src="' + cats[catID].img + '"/><div id ="contagem"><h1>' + cats[catID].name + '</h1> <h2> Contagem </h2> <p class = "cont-numb">' + cats[catID].count + '<p>');
+            clicaGato();
+        }
+        /* função clicaGato = busca o atributo cat-id da imagem e adiciona na propriedade 
+        count do objeto gato atual, depois exibe no elemento .cont-numb da página */
+    var clicaGato = function() {
+        $('#cat').find('img').click(function() {
+            var catID = $(this).attr('cat-id');
+            cats[catID].count += 1;
+            $('#cat').find('.cont-numb').html(cats[catID].count);
+        });
+    };
 });
