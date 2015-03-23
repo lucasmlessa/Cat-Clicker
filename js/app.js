d51f57603e8d832b12bc1ca87c394772a49256b0
Lucas Lessa <lucaslessa@me.com>
2015-03-23 04:11:37 -0300

    Change to Cat Clickr Double

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 $(function() {
-    var cont = 0;
-    $('#cat').click(function(e) {
-        cont +=1;
-        console.log(cont);
-        $('.cont-numb').html(cont);
+    var cats = {};
+    cats.a = 0;
+    cats.b = 0;
+    $('#cat1').click(function() {
+        cats.a += 1;
+        console.log(cats);
+        $(this).find('.cont-numb').html(cats.a);
+    });
+    $('#cat2').click(function() {
+        cats.b += 1;
+        console.log(cats);
+        $(this).find('.cont-numb').html(cats.b);
     });
 });
