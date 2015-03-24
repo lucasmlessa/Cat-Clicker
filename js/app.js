0db255d247bd535edfca17eb95143cfa0b7ab661
Lucas Lessa <lucaslessa@me.com>
2015-03-23 23:39:41 -0300

    Adding some improvements

--- js/app.js
+++ js/app.js
@@ -21,9 +21,13 @@ $(function() {
         'count': 0,
         'img': 'img/cat5.jpg',
     }];
+
+    var catMenu = $('.cat-menu');
+    var catView = $('#cat-view');
+
     /* Percorre o array cats e cria os elementos do menu .cat-menu */
     for (var i = 0; i < cats.length; i++) {
-        $('.cat-menu').prepend('<a href="#" cat-id="' + i + '" class="list-group-item">' + cats[i].name + '</a>');
+        catMenu.prepend('<a href="#" cat-id="' + i + '" class="list-group-item">' + cats[i].name + '</a>');
     };
     /* Chama a função mostraGato com base na cat-id do link clicado */
     $('.cat-menu').find('a').click(function(e) {
@@ -33,16 +37,16 @@ $(function() {
     });
     /* função mostraGato = recebe um id do gato selecionado e insere no elemento #cat da página */
     var mostraGato = function(catID) {
-            $('#cat').html('<img cat-id="' + catID + '" src="' + cats[catID].img + '"/><div id ="contagem"><h1>' + cats[catID].name + '</h1> <h2> Contagem </h2> <p class = "cont-numb">' + cats[catID].count + '<p>');
+            catView.html('<img cat-id="' + catID + '" src="' + cats[catID].img + '"/><div id ="contagem"><h1>' + cats[catID].name + '</h1> <h2> Contagem </h2> <p class = "cont-numb">' + cats[catID].count + '<p>');
             clicaGato();
         }
         /* função clicaGato = busca o atributo cat-id da imagem e adiciona na propriedade 
         count do objeto gato atual, depois exibe no elemento .cont-numb da página */
     var clicaGato = function() {
-        $('#cat').find('img').click(function() {
+        catView.find('img').click(function() {
             var catID = $(this).attr('cat-id');
             cats[catID].count += 1;
-            $('#cat').find('.cont-numb').html(cats[catID].count);
+            catView.find('.cont-numb').text(cats[catID].count);
         });
     };
 });
